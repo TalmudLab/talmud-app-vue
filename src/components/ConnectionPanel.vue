641529b9dd217a7025566fb16ec4c7c4b1c06ebe
Dan Jutan <danjutan@gmail.com>
2021-04-14 20:32:33 -0400

    connection content

--- src/components/ConnectionPanel.vue
+++ src/components/ConnectionPanel.vue
@@ -6,8 +6,12 @@
      bg-yellow-200 rounded-t-md">
 
       <h1 class="font-serif flex-grow text-center text-lg">Connections: {{connection.author}}</h1>
-
     </div>
+    <div class="text-center">
+      <h2 class="font-bold inline-block" v-html="connection.title"></h2>
+      <a class="underline ml-2 text-blue-500" :href="connection.link">(link)</a>
+    </div>
+    <div v-html="connection.text"></div>
   </div>
 </template>
 
