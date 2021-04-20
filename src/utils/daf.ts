e47cb3ba89b8a6b6ab896946c0ae79b0bec49ca1
Dan Jutan <danjutan@gmail.com>
2021-04-20 14:28:43 -0400

    refactor surrounding method

--- src/utils/daf.ts
+++ src/utils/daf.ts
@@ -5,25 +5,25 @@ export function dafEquals(daf1: daf, daf2: daf) {
   return daf1.tractate == daf2.tractate && daf1.daf == daf2.daf;
 }
 
-export function surrounding(daf: daf): {prev: daf, next: daf} {
+export function surrounding({daf, tractate}: daf): {prev: daf, next: daf} {
   const nextDaf = {
-    tractate: daf.tractate,
+    tractate,
     daf: ""
   }
   const prevDaf = {
-    tractate: daf.tractate,
+    tractate,
     daf: ""
   }
-  if (daf.daf.includes('b')) {
-    nextDaf.daf = "" + (Number(currentDaf.daf.substring(0, daf.daf.length - 1)) + 1);
+  if (daf.includes('b')) {
+    nextDaf.daf = "" + (Number(daf.substring(0, daf.length - 1)) + 1);
   } else {
-    nextDaf.daf = daf.daf + "b";
+    nextDaf.daf = daf + "b";
   }
 
-  if (daf.daf.includes('b')) {
-    prevDaf.daf = daf.daf.substring(0, daf.daf.length - 1);
+  if (daf.includes('b')) {
+    prevDaf.daf = daf.substring(0, daf.length - 1);
   } else {
-    prevDaf.daf = "" + (Number(daf.daf) - 1) + "b";
+    prevDaf.daf = "" + (Number(daf) - 1) + "b";
   }
 
   return {
@@ -33,7 +33,6 @@ export function surrounding(daf: daf): {prev: daf, next: daf} {
 }
 
 export function dafsBetween(start: daf, end: daf): Array<daf> {
-  debugger;
   if (dafEquals(start, end)) return [];
   const between = [];
   let curr = start;
