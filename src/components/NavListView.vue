7a4c90b68a95fcd9bc42839bb07ba84a2f1c5213
Dan Jutan <danjutan@gmail.com>
2021-04-22 23:40:12 -0400

    unexpand sentence on page switch

--- src/components/NavListView.vue
+++ src/components/NavListView.vue
@@ -36,13 +36,13 @@
       <div
         :style="{width: `calc(96% - ${render.indent + (dragging == index)}*1rem)`}">
 
-        <div v-if="expanded.has(index)" class="flex" :class="{'flex-row-reverse': !english}">
+        <div v-if="expanded.has(render)" class="flex" :class="{'flex-row-reverse': !english}">
           <div class="flex flex-col" :class="{'flex-col-reverse': !english}">
             <div v-if="english" class="text-sm" v-html="render.sentence.en"></div>
             <div v-else class="rtl text-right" v-html="render.sentence.he"></div>
           </div>
           <div class="self-start float-right">
-            <a @click="expanded.delete(index)">
+            <a @click="expanded.delete(render)">
               <i class="fas text-xl fa-caret-down"></i>
             </a>
           </div>
@@ -51,7 +51,7 @@
              :class="{'flex-row-reverse': !english}">
           <div class="text-sm flex-grow font-semibold truncate" :class="{rtl: !english}" v-html="english ? render.shortEn : render.shortHe">
           </div>
-          <a v-show="hovered == index" @click="expanded.add(index)">
+          <a v-show="hovered == index" @click="expanded.add(render)">
             <i class="fas text-xl"  :class="[english ? 'fa-caret-right' : 'fa-caret-left']"></i>
           </a>
         </div>
